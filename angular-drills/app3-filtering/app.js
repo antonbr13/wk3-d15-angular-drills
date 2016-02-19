fc24a560a144b32b06215519442f60026bda664d
Anton Bredl <antonbr13@gmail.com>
2016-02-19 11:45:19 -0700

    added new filter functionality

---
+++ angular-drills/app3-filtering/app.js
@@ -0,0 +1,1 @@
+var ngFilter = angular.module('filterApp', []);
---
+++ angular-drills/app3-filtering/controller.js
@@ -0,0 +1,11 @@
+ngFilter.controller('filterCtrl', function($scope, filterService){
+
+$scope.data = filterService.getData();
+
+$scope.filterObj = {
+   email: '.ca',
+   phone: '1234'
+};
+
+
+});
