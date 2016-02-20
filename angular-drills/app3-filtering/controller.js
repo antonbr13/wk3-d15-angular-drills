ec013b089051601d42ceeb65e736efcc01acb7de
Anton Bredl <antonbr13@gmail.com>
2016-02-20 14:12:08 -0700

    just inspecting code

--- angular-drills/app3-filtering/controller.js
+++ angular-drills/app3-filtering/controller.js
@@ -2,10 +2,10 @@ ngFilter.controller('filterCtrl', function($scope, filterService){
 
 $scope.data = filterService.getData();
 
-$scope.filterObj = {
-   email: '.ca',
-   phone: '1234'
-};
+// $scope.filterObj = {
+//    email: '.ca',
+//    phone: '1234'
+// };
 
 
 });
